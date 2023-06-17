5a6b824661c3c283b3d4d4bb29798c6e8406daba
Hani-Shaif <mohsenhani1991@gmail.com>
2023-06-17 21:36:40 +0200

    Add Python_Basic_variables

---
+++ 02.Python_Basic_Variabes.py
@@ -0,0 +1,68 @@
+#=====================================
+#=======Python_Basic_examples=========
+#======Python Basic Variables=========
+#=====================================
+
+# a variable is name that refers to a value [ variable = variable name + variable value ]
+# python is Dynamically Typed Language
+  # We don't have to declare the type of variable while assigning a value to a variable in Python.
+
+# Rules for Python variables:
+  # A variable name must start with a letter or the underscore character
+  # A variable name cannot start with a number
+  # A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+  # Variable names are case-sensitive (age, Age and AGE are three different variables)
+  # A variable name cannot be any of the Python keywords.
+
+# Python Data Types
+  # int     => [-1 , 0 , 1]
+  # str     => name = "Hani"
+  # float   => [-2.2, 3.5]
+  # bool    => True , False
+  # list    => [1,2,3,4,5] //// Can br edited
+  # tuple   => (10,11,12,13,14,15) //// Can not be edited ((constant))
+  # dict    => {1: "Hani", 2: "Ali", 3: "Hassan"} // {key: value}
+
+# Type method : use the type() method to know the type of the variable
+x = "Welcome"
+print(type(x)) # // str
+myList = [1,2,3,4]
+print(type(myList)) # // list
+myTuple = ("Hello")
+print(type(myTuple)) ## // str
+myTuple2 = ("Hello",)
+print(type(myTuple2)) ## // str
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
