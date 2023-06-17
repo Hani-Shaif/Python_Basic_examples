257574600e0791abd4ab8cb7d41e8fd5e6f04417
Hani-Shaif <mohsenhani1991@gmail.com>
2023-06-17 21:38:15 +0200

    Add Python_Basic_Operators

---
+++ 03.Python_Basic_Operators.py
@@ -0,0 +1,105 @@
+#=====================================
+#=======Python_Basic_examples=========
+#========= Python Operators ==========
+#=====================================
+
+# Python Operators
+  # Arithmetic operators
+  # Comparison operators
+  # Assignment operators
+  # Logical operators
+
+# ===== Arithmetic operators =====
+  # + Addition
+  # - subtraction
+  # * multiplication
+  # / division
+  # % modulus
+  # // floor division[ floor division 9 // 2 = 4 ]
+  # ** exponent 2**4 = 2*2*2*2 = 16 
+  
+x = 9 / 3
+print(type(x)) # = 3.0  => float
+print(type(int(x))) # = 3 => int
+ 
+y = 9 // 2  # = 4 => int
+print(type(y)) # int
+
+x = "Hani " + "Ali"
+print(x)
+y = "10" + "20"
+print(y) #  1020
+    #   y = "10" + "20"
+    #   print(y) # TypeErro => not division 'str' / 'str' 
+
+# ===== Comparison operators =====
+  # == equal
+  # != not equal  => x != 10 => Is x = 10? //question
+  # >  greater then
+  # < less than
+  # >= greater than or equal to
+  # <= less than or eqaul to
+
+# ===== Assignment operators =====
+  # = assign  value =>  x = 10
+  # += add and assignment
+  # -= subtract and assignment
+  # *= multiply and assignment
+  # /= divide and assignment
+  # //= floor division and  assignment
+  # %= modulus and assignment
+  # **= exponent and assignment
+
+# ====== Logical operators ======
+  # and = Returns True if both statements are true => x == 2 and y == 3
+  # or  = Returns True if one of the statements is true => x == 2 or y == 3
+  # not = Reverse the result, returns False if the result is true => not(x < 5 and x < 10)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
